926b329b49ab5a80fc692cfb4d80b788c8e4c331
giri adini <giriadini@giris-MacBook-Air.local>
2016-09-15 01:49:58 +0530

    Added code to load Json Files and join  the  spark RDD's

--- BookingService/src/main/scala/BookingService.scala
+++ BookingService/src/main/scala/BookingService.scala
@@ -1,7 +1,39 @@
-class BookingService{
-}
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 
 object BookingCustomer{
+
+
 def main(args:Array[String]]){
+
+ val conf = new SparkConf().setAppName("BookingCustomer").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val bookingDetails = sqlContext.read.json("/Users/giriadini/Documents/HolidayCheck/challenge/f1.json")
+	
+	val bd_destination = bookingDetails("destination")
+	val bd_buId = bookingDetails("buId")
+
+
+	// val filtervalues = bookingDetails.filter(bd_destination_column.!==("") && bd_buId_column.>("40"))
+	
+	val bd_destination_filter = bookingDetails.filter(bd_destination.!==("") )
+
+	 
+	 val transactionDetails = sqlContext.read.json("/Users/giriadini/Documents/HolidayCheck/challenge/f2.json")
+	 
+	val ts_logId = transactionDetails("logId")
+	val ts_buId = transactionDetails("buId")
+	val ts_price = transactionDetails("price")
+	val ts_datetime = transactionDetails("datetime")
+	
+	
+	//val bookingTransactions  = bookingDetails.join(transactionDetails, bookingDetails.col("buId") === transactionDetails.col("buId"))
+
+	val bookingTransactions  = bd_destination_filter.join(transactionDetails)
+
+	bookingTransactions.show()
 
 }
